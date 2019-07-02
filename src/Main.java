ed34b6583bb169cf30eb683d78c19f981785e6f0
adonayab <adonayab15@hotmail.com>
2019-07-01 21:18:01 -0500

    Some name changes

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Card card1 = new Card("Fav1", "Visa", 100);
-        Card card2 = new Card("Fav2", "MC", 100);
-        Card card3 = new Card("Fav3", "Disc", 100);
+        Card card1 = new Card("Fav Visa", "Visa", 100);
+        Card card2 = new Card("Fav MC", "MC", 100);
+        Card card3 = new Card("Fav Disc", "Disc", 100);
 
         Wallet wal1 = new Wallet("Fav Wallet");
         wal1.addCard(card1);
@@ -15,15 +15,15 @@ public class Main {
         p1.addWallet(wal1);
         p1.interestView();
 
-        Card card4 = new Card("Fav1", "Visa", 100);
-        Card card5 = new Card("Fav2", "MC", 100);
-        Card card6 = new Card("Fav3", "Disc", 100);
+        Card card4 = new Card("Fav Visa", "Visa", 100);
+        Card card5 = new Card("Fav MC", "MC", 100);
+        Card card6 = new Card("Fav Disc", "Disc", 100);
 
-        Wallet wal2 = new Wallet("Another Fav Wallet");
+        Wallet wal2 = new Wallet("Fav Wallet");
         wal1.addCard(card4);
         wal2.addCard(card6);
 
-        Wallet wal3 = new Wallet("Cat Wallet");
+        Wallet wal3 = new Wallet("Another Fav Wallet");
         wal3.addCard(card5);
 
         Person p2 = new Person("Mark");
@@ -31,16 +31,16 @@ public class Main {
         p2.addWallet(wal3);
         p2.interestView();
 
-        Card card7 = new Card("Fav1", "Visa", 100);
-        Card card8 = new Card("Fav2", "MC", 100);
-        Card card9 = new Card("Fav1", "Visa", 100);
-        Card card10 = new Card("Fav2", "MC", 100);
+        Card card7 = new Card("Fav Visa", "Visa", 100);
+        Card card8 = new Card("Fav MC", "MC", 100);
+        Card card9 = new Card("Fav Visa", "Visa", 100);
+        Card card10 = new Card("Fav MC", "MC", 100);
 
-        Wallet wal4 = new Wallet("Another Fav Wallet");
+        Wallet wal4 = new Wallet("Fav Wallet");
         wal4.addCard(card7);
         wal4.addCard(card8);
 
-        Wallet wal5 = new Wallet("Cat Wallet");
+        Wallet wal5 = new Wallet("Fav Wallet");
         wal5.addCard(card9);
         wal5.addCard(card10);
 
