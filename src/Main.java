2739b60e868f1df130ff167ffa202ee734f02ea7
adonayab <adonayab15@hotmail.com>
2019-07-01 21:14:11 -0500

    Improved more

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,56 @@ public class Main {
     public static void main(String[] args) {
 
         Card card1 = new Card("Fav1", "Visa", 100);
-        Card card2 = new Card("Fav2", "MC", 50);
-        Card card3 = new Card("Fav3", "Disc", 230);
-
-        Card card4 = new Card("Fav4", "Visa", 23.45);
-        Card card5 = new Card("Fav5", "MC", 300);
-        Card card6 = new Card("Fav6", "Disc", 456.45);
+        Card card2 = new Card("Fav2", "MC", 100);
+        Card card3 = new Card("Fav3", "Disc", 100);
 
         Wallet wal1 = new Wallet("Fav Wallet");
         wal1.addCard(card1);
         wal1.addCard(card2);
-
-        Wallet wal2 = new Wallet("Another Fav Wallet");
         wal1.addCard(card3);
-        wal2.addCard(card5);
-
-
-        Wallet wal3 = new Wallet("Cat Wallet");
-        wal3.addCard(card4);
-        wal3.addCard(card6);
 
         Person p1 = new Person("John");
         p1.addWallet(wal1);
-        p1.addWallet(wal2);
+        p1.interestView();
+
+        Card card4 = new Card("Fav1", "Visa", 100);
+        Card card5 = new Card("Fav2", "MC", 100);
+        Card card6 = new Card("Fav3", "Disc", 100);
+
+        Wallet wal2 = new Wallet("Another Fav Wallet");
+        wal1.addCard(card4);
+        wal2.addCard(card6);
+
+        Wallet wal3 = new Wallet("Cat Wallet");
+        wal3.addCard(card5);
 
         Person p2 = new Person("Mark");
+        p2.addWallet(wal2);
         p2.addWallet(wal3);
-
-        p1.interestView();
         p2.interestView();
+
+        Card card7 = new Card("Fav1", "Visa", 100);
+        Card card8 = new Card("Fav2", "MC", 100);
+        Card card9 = new Card("Fav1", "Visa", 100);
+        Card card10 = new Card("Fav2", "MC", 100);
+
+        Wallet wal4 = new Wallet("Another Fav Wallet");
+        wal4.addCard(card7);
+        wal4.addCard(card8);
+
+        Wallet wal5 = new Wallet("Cat Wallet");
+        wal5.addCard(card9);
+        wal5.addCard(card10);
+
+        Person p3 = new Person("Mercy");
+        p3.addWallet(wal4);
+        p3.interestView();
+
+        Person p4 = new Person("Hanna");
+        p4.addWallet(wal5);
+        p4.interestView();
+
+
 
     }
 }
